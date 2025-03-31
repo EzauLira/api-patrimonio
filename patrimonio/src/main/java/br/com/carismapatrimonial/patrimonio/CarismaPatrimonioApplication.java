a760fcd644777eb55b72ce542d1d711cc35c4ac9
Ezau Miguel Lira <elira@foursys.com.br>
2025-03-31 15:51:43 -0300

    Inicio

---
+++ patrimonio/src/main/java/br/com/carismapatrimonial/patrimonio/CarismaPatrimonioApplication.java
@@ -0,0 +1,13 @@
+package br.com.carismapatrimonial.patrimonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarismaPatrimonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarismaPatrimonioApplication.class, args);
+	}
+
+}
---
+++ patrimonio/src/test/java/br/com/carismapatrimonial/patrimonio/CarismaPatrimonioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carismapatrimonial.patrimonio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarismaPatrimonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
